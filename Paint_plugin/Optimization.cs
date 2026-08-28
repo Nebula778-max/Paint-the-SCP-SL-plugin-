e9b22c4da4df78a4bad24bf76a11d112a3e38440
Nebula <Levcenkopavel154@gmail.com>
2026-08-28 15:43:25 +0300

    1.0.1 version

---
+++ Paint_plugin/Optimization.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using Exiled.API.Features;
+using System.Collections.Generic;
+
+namespace SCPCanvasPaint
+{
+    public class CanvasTriggerZone : MonoBehaviour
+    {
+        public CanvasInstance Canvas;
+        public static HashSet<Player> PlayersInZone = new HashSet<Player>();
+
+        private void OnTriggerEnter(Collider other)
+        {
+            Player player = Player.Get(other.gameObject);
+            if (player != null && !PlayersInZone.Contains(player))
+            {
+                PlayersInZone.Add(player);
+
+                if (Plugin.Singleton.CanvasManager.Sessions.TryGetValue(player, out var session) && session.IsDrawing)
+                {
+                    Plugin.Singleton.EventHandler.CheckAndStartRaycast(player, session);
+                }
+            }
+        }
+
+        private void OnTriggerExit(Collider other)
+        {
+            Player player = Player.Get(other.gameObject);
+            if (player != null)
+            {
+                PlayersInZone.Remove(player);
+            }
+        }
+    }
+}
